2c2ae7d0f56c1d67d8dbac27b691aad0ce5d1530
Nikita Ibragimov <nik.ibragimov@gmail.com>
2017-02-16 10:11:05 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,40 +8,7 @@ app = Flask(__name__)
 mysql=MySQL(app)
 
 
-"""
-app.config['MYSQL_DATABASE_USER'] = 'Slavin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'World202web'
-app.config['MYSQL_DATABASE_DB'] = 'Slavin$Inventory'
-app.config['MYSQL_DATABASE_HOST'] = 'Slavin.mysql.pythonanywhere-services.com'
-mysql.init_app(app)
 
-db = mysql.connect(host="Slavin.mysql.pythonanywhere-services.com", user="Slavin", passwd="Google103", db="Slavin$Inventory")
-cur = db.cursor()
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-@app.route('/view/', methods=["POST", "GET"])
-def view():
-    if request.method == 'POST':
-        invid = request.form['invid']
-        return render_template("view.html", invid = invid)
-
-@app.route('/enter/', methods=["GET", "POST"])
-def enter():
-    return render_template("enter.html")
-
-@app.route("/test/")
-def student():
-   return render_template("student.html")
-
-@app.route('/result/', methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      return render_template("result.html",result = result)
-
-"""
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -53,7 +20,7 @@ def view():
 @app.route("/enter", methods=['GET', 'POST'])
 def enter():
     try:
-        con = mysql.connect("Slavin.mysql.pythonanywhere-services.com","Slavin","Google103","Slavin$Inventory" )
+        con = mysql.connect("xxxx","xxx","xxxx","xxxx" )
         things = []
         with con:
             cur = con.cursor()
@@ -69,7 +36,7 @@ def enter():
 @app.route("/add", methods=['GET', 'POST'])
 def adding():
     try:
-        con = mysql.connect("Slavin.mysql.pythonanywhere-services.com","Slavin","Google103","Slavin$Inventory" )
+        con = mysql.connect("xxxx","xxx","xxxx","xxxx" )
         things = []
         with con:
             cur = con.cursor()
@@ -82,15 +49,8 @@ def adding():
             return render_template("enter.html")
     except Exception as e:
         return(str(e))
-"""
-@app.route("/echo", methods=['POST'])
-def echo():
-    text=request.form['text']
-    cur.execute("SELECT * FROM Item WHERE invid = text")
-    data = cur.fetchall()
-    db.close
-    return render_template('echo.html', text = data)
-"""
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
